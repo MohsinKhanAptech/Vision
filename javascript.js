87618ad2b30ee0f36caabd643afb54547afee840
Mohsin <muhammadmohsinkhan.aptech@gmail.com>
2023-08-21 04:17:10 +0500

    fixed some dark theme errors

--- javascript.js
+++ javascript.js
@@ -79,11 +79,13 @@ function changeTheme() {
     if (theme == 'light') {
         document.documentElement.style.cssText =
             "--accent-color-1: #6a3ed5;--accent-color-2: #ff148a;--white-15: rgba(45, 45, 45, 0.15);--white-25: rgba(45, 45, 45, 0.25);--white-50: rgba(45, 45, 45, 0.50);--white-75: rgba(45, 45, 45, 0.75);--white-100: rgba(45, 45, 45, 1);--black-50: rgba(235, 235, 235, 0.50);--black-100: rgba(235, 235, 235, 1);--logo-light: url(assets/logo\ dark.png);--logo-dark: url(assets/logo\ light.png);";
+        document.getElementById("logoImg").classList.add("dark")
         localStorage.theme = "dark";
         theme = localStorage.theme;
     } else {
         document.documentElement.style.cssText =
             "--accent-color-1: #7f59db;--accent-color-2: #ff69b4;--white-15: rgba(255, 255, 255, 0.15);--white-25: rgba(255, 255, 255, 0.25);--white-50: rgba(255, 255, 255, 0.50);--white-75: rgba(255, 255, 255, 0.75);--white-100: rgba(255, 255, 255, 1);--black-50: rgba(0, 0, 0, 0.50);--black-100: rgba(0, 0, 0, 1);--transparent-color: rgba(0, 0, 0, 0);--logo-light: url(assets/logo\ light.png);--logo-dark: url(assets/logo\ dark.png);";
+        document.getElementById("logoImg").classList.remove("dark")
         localStorage.theme = "light";
         theme = localStorage.theme;
     }
